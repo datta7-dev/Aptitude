a6cce54e6737d3f73c87301eeaaa53a9c1319f26
datta7-dev <72849481+datta7-dev@users.noreply.github.com>
2020-11-02 19:20:52 +0530

    Update settings.py

--- aptitude/settings.py
+++ aptitude/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6mn=09z7yqzr50i*xdon*op65%)kih1_^lce9aa2f1^t!d4ndy'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
